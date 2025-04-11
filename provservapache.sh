4a6cd35ed95634564a6d5b516f876da7011b9cc6
André Pimenta Fonseca <127700053+AndrePimentaF@users.noreply.github.com>
2025-04-11 16:26:04 -0300

    Segundo exercicio Santander Open Academy

---
+++ provservapache.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+
+#=======================#
+# Provisionamento de servidor Apache
+# PsA v1.0
+# 2025-04-11 - Primeiro modelo
+# !Atenção! -Precisa estar logado como root para executar este #programa
+# !CUIDADO! -Alterar apenas o nome do gerenciador de pacotes caso não utilize distribuições baseadas em Debian(apt)
+#=======================#
+
+echo"Limpa cache, atualiza servidor e instala: apache,unzip e wget "
+apt clean
+apt-get update && upgrade -y
+apt-get install apache2 unzip -y
+apt-get install wget -y
+
+echo "Baixa e copia arquivos"
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "Finalizado!"
+
+
+
